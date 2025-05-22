cc8f9d42e0c18cfe51e43fb980d9efd74829b7b4
Utkarsh Raj Srivastava <utkarshrajsrivastava7@gmail.com>
2025-05-23 00:39:09 +0530

    login made

---
+++ app/login/action.ts
@@ -0,0 +1,24 @@
+'use server'
+
+import { supabaseServer } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
+
+
+export async function login(prevState: unknown, formData: FormData) {
+  const supabase = await supabaseServer()
+
+  const email = (formData.get('email') as string)?.trim().toLowerCase()
+  const password = (formData.get('password') as string)?.trim()
+
+  if (!email || !password) {
+    return { message: 'Email and password are required.' }
+  }
+
+  const { error } = await supabase.auth.signInWithPassword({ email, password })
+
+  if (error) {
+    return { message: error.message }
+  }
+
+  redirect('/chats')
+}
