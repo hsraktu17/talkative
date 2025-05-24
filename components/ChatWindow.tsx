e60096a5ab32314891c71682e7766b8ed9a2a442
Utkarsh Raj Srivastava <utkarshrajsrivastava7@gmail.com>
2025-05-24 14:30:52 +0530

    chat is working but like http server not like websocket

--- components/ChatWindow.tsx
+++ components/ChatWindow.tsx
@@ -85,7 +85,7 @@ export default function ChatWindow({ chat, currentUser }: ChatWindowProps) {
               </div>
             </div>
           ))
-        )}
+        )} 
         <div ref={messagesEndRef} />
       </div>
       <form
